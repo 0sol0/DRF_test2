e7018bf69156c2ada133a5905db982dda08cecef
0sol0 <jdsmy1004@gmail.com>
2022-09-27 10:45:28 +0900

    Update file

--- introduce/models.py
+++ introduce/models.py
@@ -5,6 +5,10 @@ class AccessLog(models.Model):
     class Meta:
         db_table = 'AccessLog'
     
-    created_at = models.DateTimeField(auto_now_add=True)
-    location = models.CharField(max_length=256, default='')
+    created_at = models.DateTimeField('접속 시간', auto_now_add=True)
+    # updated_at = models.DateTimeField('수정 시간', auto_now=True)
+    location = models.CharField('접속 경로', max_length=256)
+        
+    def __str__(self):
+        return f'{self.created_at}  {self.location}'
     
--- introduce/views.py
+++ introduce/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from . import models
+from introduce.models import AccessLog
 
 # Create your views here.
 def introduce_view(request):
+    
+    access_log = AccessLog()
+    access_log.location = 'introduce'
+    access_log.save()
+    
     return render(request, 'index.html')
+
+    # AccessLog.objects.create(
+    #     location = 'introduce'
+    # )
+    
